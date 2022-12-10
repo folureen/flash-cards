c74ee055697ffb3c282a6fd5434915aa828524bb
folureen <folureen@gmail.com>
2022-12-10 19:37:49 +0300

    added webpack config, redux-toolkit and sagas

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "none",
+  entry: {
+    app: path.join(__dirname, "src", "index.tsx"),
+  },
+  target: "web",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    alias: {
+      src: path.resolve(__dirname, "src/"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts)x?$/,
+        exclude: /node_modules|\.d\.ts$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            compilerOptions: {
+              noEmit: false,
+            },
+          },
+        },
+      },
+    ],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};
